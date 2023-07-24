bc96518c0bbb76ec94efdad02451767cb50b9292
Hoon Wee <mrgravity817@gmail.com>
2023-07-24 23:34:15 +0900

    feat: show individual user by id

--- lib/rumbl_web/live/users_live.ex
+++ lib/rumbl_web/live/users_live.ex
@@ -6,26 +6,54 @@ defmodule RumblWeb.UsersLive do
     users = Accounts.list_users()
 
     socket =
-      assign(socket, users: users)
+      assign(socket, users: users, selected_user: nil)
 
     # should return this tuple
     {:ok, socket}
   end
 
+  # handle /users/:id
+  def handle_params(%{"id" => id}, _uri, socket) do
+    user = Accounts.get_user(id)
+
+    {:noreply, assign(socket, selected_user: user)}
+  end
+
+  # handle /users
+  def handle_params(_params, _uri, socket) do
+    {:noreply, socket}
+  end
+
   def render(assigns) do
     ~H"""
-    <h1 class="text-xl mb-4">Listing users</h1>
-    <table>
-      <%= for user <- @users do %>
-        <tr>
-          <td>
-            <.link href={~p"/users/#{user.id}"}>
-              <b><%= first_name(user) %></b> (<%= user.id %>)
-            </.link>
-          </td>
-        </tr>
-      <% end %>
-    </table>
+    <%= if @selected_user do %>
+      <.user user={@selected_user} socket={@socket} />
+    <% else %>
+      <h1 class="text-xl mb-4">Listing users</h1>
+      <table>
+        <%= for user <- @users do %>
+          <tr>
+            <td>
+              <.link href={~p"/users/#{user.id}"}>
+                <b><%= first_name(user) %></b> (<%= user.id %>)
+              </.link>
+            </td>
+          </tr>
+        <% end %>
+      </table>
+    <% end %>
+    """
+  end
+
+  attr :user, :map, required: true
+  attr :socket, :map, required: true
+
+  def user(assigns) do
+    ~H"""
+    <h1 class="text-xl mb-4">Showing User</h1>
+    <p>
+      <b><%= first_name(@user) %></b> (<%= @user.id %>)
+    </p>
     """
   end
 
