adcffdb437155898895483d3797ccb99383f6418
Hoon Wee <mrgravity817@gmail.com>
2023-07-24 23:57:35 +0900

    feat: update resource API to use Ecto

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -2,26 +2,22 @@ defmodule Rumbl.Accounts do
   @moduledoc """
   The Accounts context
   """
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
-  def list_users do
-    [
-      %User{id: "1", name: "Hoon", username: "hoonweedev"},
-      %User{id: "2", name: "Eunbee", username: "fiona917"},
-      %User{id: "3", name: "Jay", username: "jaywee626"}
-    ]
+  def get_user(id) do
+    Repo.get(User, id)
   end
 
-  def get_user(id) do
-    list_users()
-    |> Enum.find(&(&1.id == id))
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} ->
-        Map.get(map, key) == val
-      end)
-    end)
+    Repo.get_by(User, params)
+  end
+
+  def list_users do
+    Repo.all(User)
   end
 end
